175f93de265e5c6a2671bd76f9d10ac8a408738d
Bentouati Mehdi <mehdi.bentouati@docaposte.fr>
2024-12-02 13:49:14 +0100

    feat: tmp

--- day02/01/puzzle1.go
+++ day02/01/puzzle1.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
-
-type Report = []int;
 
 func main() {
 	file, err := os.Open("../input.txt")
@@ -16,10 +16,39 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	report := make([]Report, 0)
+	safeReport := 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		lineSplit := strings.Split(line, " ")
 
+		// determine first trend
+		firstElem, _ := strconv.Atoi(lineSplit[0])
+		secElem, _ := strconv.Atoi(lineSplit[1])
+		bufTrend := firstElem - secElem
+
+		//if len(lineSplit) == 2 && bufTrend >= -3 && bufTrend <= 3 {
+		//	safeReport += 1
+		//	continue
+		//}
+
+		safeLine := true
+		for index := 2; index < len(lineSplit); index++ {
+			curElem, _ := strconv.Atoi(lineSplit[index])
+			prevElem, _ := strconv.Atoi(lineSplit[index-1])
+			curTrend := prevElem - curElem
+
+
+			if curTrend < -3 && curTrend > 3  || curTrend * bufTrend <= 0 || curTrend == 0 {
+				safeLine = false
+				break
+			}
+		}
+
+		if (safeLine) {
+			safeReport += 1
+		}
+
 	}
+	fmt.Println("safe report : ", safeReport)
+
 }
