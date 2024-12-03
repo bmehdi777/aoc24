6463dd04c5b2b6e14561611a4b17f4e94f938cc1
Bentouati Mehdi <mehdi.bentouati@docaposte.fr>
2024-12-03 08:53:42 +0100

    feat: day03-01/2

---
+++ day03/01/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"unicode"
+)
+
+func main() {
+	file, err := os.ReadFile("../input.txt")
+	if err != nil {
+		panic(err)
+	}
+	fileString := string(file)
+
+	res := 0
+	for pos, char := range fileString {
+		if unicode.IsDigit(char) {
+			lookupIndex := pos
+			num1 := ""
+			num2 := ""
+			num3 := 0
+			if fileString[pos-4:pos] == "mul(" {
+				for unicode.IsDigit(rune(fileString[lookupIndex])) {
+					num1 += string(fileString[lookupIndex])
+					lookupIndex++
+				}
+				if num1 == "" {
+					pos = lookupIndex
+					continue
+				}
+
+				fmt.Println("num1 = ", num1)
+				if fileString[lookupIndex] == ',' {
+					lookupIndex++
+					for unicode.IsDigit(rune(fileString[lookupIndex])) {
+						num2 += string(fileString[lookupIndex])
+						lookupIndex++
+					}
+
+					if num2 == "" {
+						// invalid
+						pos = lookupIndex
+						continue
+					}
+
+					fmt.Println("num2 = ", num2)
+
+					if fileString[lookupIndex] == ')' {
+						num1, err := strconv.Atoi(num1)
+						if err != nil {
+							panic(err)
+						}
+
+						num2, err := strconv.Atoi(num2)
+						if err != nil {
+							panic(err)
+						}
+
+						fmt.Printf("mul(%v,%v)\n", num1, num2)
+						num3 = num1 * num2
+						res += num3
+						pos = lookupIndex
+					}
+				} else {
+					pos = lookupIndex
+				}
+			} else {
+				pos = lookupIndex
+			}
+		}
+	}
+
+	fmt.Println("Result : ", res)
+}
