48bc0e5a84c66bbf505b4065ab6e094092027f91
Bentouati Mehdi <mehdi.bentouati@docaposte.fr>
2024-12-09 09:06:37 +0100

    feat: add 07

---
+++ day07/01/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("../input.txt")
+	//file, err := os.Open("../example.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	trackNode := make([]Node, 0)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lineSplit := strings.Split(line, ": ")
+
+		finalRes, _ := strconv.Atoi(lineSplit[0])
+		numbers := strings.Split(lineSplit[1], " ")
+
+		num, _ := strconv.Atoi(numbers[0])
+		head := Node{Value: num}
+
+		for i := 1; i < len(numbers); i++ {
+			num, _ = strconv.Atoi(numbers[i])
+			head.AddNode = &Node{Value: head.Value + num}
+			head.MulNode = &Node{Value: head.Value * num}
+			
+		}
+	}
+
+}
+
+type Node struct {
+	Value   int
+	AddNode *Node
+	MulNode *Node
+}
